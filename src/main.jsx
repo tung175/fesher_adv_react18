be2f2a225a5e21458c94d10a223cfcc92b38ad8f
tung175 <duongkq3@gmail.com>
2024-08-28 01:07:58 +0700

    init project

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>Error page</div>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
---
+++ src/pages/book/index.jsx
@@ -0,0 +1,7 @@
+const BookPage = () => {
+    return (
+        
+    )
+}
+
+export default BookPage
